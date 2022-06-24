6ee8eb55db1a63bf6021136dd60868a444ad60f9
Ashwin Rachha <ashwin.rachha@outreach.io>
2022-06-24 17:52:07 -0400

    wrapper

---
+++ wrapper/client/pred-client.go
@@ -0,0 +1,9 @@
+package client
+
+import (
+	"fmt"
+)
+
+func ClientOK() {
+	fmt.Println("This is Client")
+}
---
+++ wrapper/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"triton-inference-baseline/wrapper/client"
+)
+
+func main() {
+	fmt.Println("This is main")
+}
